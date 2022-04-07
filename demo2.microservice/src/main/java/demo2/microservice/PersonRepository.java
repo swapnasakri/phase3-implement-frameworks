4b01d17d279fea0a786f53d0c2427acab0db75e1
swapnasakri <99816650+swapnasakri@users.noreply.github.com>
2022-04-07 20:13:51 +0530

    Add files via upload

---
+++ demo2.microservice/src/main/java/demo2/microservice/PersonRepository.java
@@ -0,0 +1,10 @@
+package demo2.microservice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends JpaRepository<PersonEntity, Integer> {
+
+}
+
